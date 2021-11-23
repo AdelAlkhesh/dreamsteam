439a3d048075c17a23bccc1d064d8d867908b771
Nicolas Molina <nicomg100@gmail.com>
2021-11-23 12:13:37 +0100

    starting with inserting game data

--- routes/game-routes.js
+++ routes/game-routes.js
@@ -3,36 +3,39 @@ const { default: axios } = require("axios")
 const router = require("express").Router()
 
 
+
 router.get("/games", (req, res, next) => {
 
-    axios.get(`https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.API_KEY}&max_results=2`)
+    axios.get(`https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.API_KEY}&max_results=10
+    `)
         .then((gameList) => {
             let gameId = gameList.data.response.apps
             let gameArr = []
+            let games = []
             let myPromises = []
 
             gameId.forEach((game, i) => {
                 myPromises.push(axios.get(`https://store.steampowered.com/api/appdetails?appids=${gameId[i].appid}`))
             });
             Promise.allSettled(myPromises)
-            .then((gameResponse) => {
-                //not complete
-                console.log(gameResponse)
-                    let gameInfo = gameResponse.data
-                    let name = Object.keys(gameInfo)[0]
+                .then((gameResponse) => {
 
-                    // gameArr = gameArr.push(gameInfo[name].data)
-                    // res.render("games.hbs", {games: gameArr})
-                    // console.log(gameArr, "inside loop");
+                    gameResponse.forEach((elem, i) => {
+                        let gameInfo = elem.value.data
+                        gameArr.push(gameInfo)
+                    })
 
-                    // console.log(gameStats);
-                    // return gameStats
-                    res.render("games.hbs", {games: gameArr})
+                    gameArr.forEach((gameObj, i) => {
+                        let name = Object.keys(gameObj)
+                        games.push(gameObj[name])
+                        })
+
+                    // console.log(games[0].data.name);
+                    res.render("games.hbs", {games})
+
                 }).catch((err) => {
                     next(err)
                 });
-            // console.log(gameArr, "outside loop");
-           
             
         }).catch((err) => {
             next(err)
@@ -55,8 +58,6 @@ router.get("/games/:id", (req, res, next) => {
         next(err)
     });
 
-    
-    // res.render("game-details.hbs",{id})
 })
 
 
