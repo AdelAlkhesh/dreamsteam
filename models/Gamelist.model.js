0939b0d7e7a451a105e0a820a914fcef1c937f16
AdelAlkhesh <splinteredsanity@outlook.com>
2021-11-23 14:58:51 +0100

    css changes. navbar added

--- models/Gamelist.model.js
+++ models/Gamelist.model.js
@@ -1,8 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const gameListSchema = new Schema(
-  {
+    {
+      
         steam_appid: Number,
+        name: String, 
         data: {
             type: [Schema.Types.ObjectId],
             ref: 'Games'
