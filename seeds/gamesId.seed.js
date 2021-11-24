63914e58e27b9099e8f59f77f8478b82f535399b
Nicolas Molina <nicomg100@gmail.com>
2021-11-24 09:32:41 +0100

    idk

--- seeds/gamesId.seed.js
+++ seeds/gamesId.seed.js
@@ -3,17 +3,21 @@ const mongoose = require("mongoose")
 const GamesIdModel = require("../models/Games-id.model")
 const axios = require("axios")
 
-axios.get(`https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${process.env.API_KEY}&max_results=5`)
+axios.get(`https://api.steampowered.com/IStoreService/GetAppList/v1/?key=6CFD1621C5C18DE7771DF6C579BF2C25`)
     .then((gameList) => {
-        let gameId = gameList.data.response.apps
-        console.log(gameId)
-        return GamesIdModel.insertMany(gameList)
+        let gameData = gameList.data.response.apps
+        let results = 
+
+        gameData.forEach(element => {
+            GamesIdModel.create({apps: element})
+        });
+        
     })
     .then(() => {
         console.log("data inserted")
         mongoose.connection.close
     })
     .catch((err) => {
-        console.log("Problems", err)
+        console.log("Problemssssssssssssssssss")
         mongoose.connection.close
     });
