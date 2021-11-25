87cd89a9eb85c110a67a4789db73aa1401cfd6b5
Nicolas Molina <nicomg100@gmail.com>
2021-11-25 12:32:42 +0100

    idk

--- routes/list-routes.js
+++ routes/list-routes.js
@@ -6,7 +6,7 @@ router.get("/lists", (req, res, next) => {
     GameList.find()
     .populate()
     .then((lists) => {
-        console.log(lists);
+
         res.render("../views/lists.hbs", {lists})
     }).catch((err) => {
         
